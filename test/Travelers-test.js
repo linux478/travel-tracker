a7c66da0e7c1072ed034cd765986c0dc6c6ada00
Susanna Carey <susannaopal@gmail.com>
2021-11-13 14:54:39 -0500

    Write remaining traveler tests for information for ID, Name, and Traveler type

--- test/Travelers-test.js
+++ test/Travelers-test.js
@@ -14,15 +14,29 @@ describe('Traveler', () => {
       "travelerType": "foodie"
     };
     traveler = new Traveler(travelerData);
-    })
+  });
 
     it('should be a function', function() {
       expect(Traveler).to.be.a('function');
-    })
+    });
 
     it('should instantiate a Travler', function () {
-      expect(traveler).to.be.an.instanceOf(Traveler)
-    })
+      expect(traveler).to.be.an.instanceOf(Traveler);
+    });
+
+    it('should have a id', function() {
+      expect(traveler.id).to.be.equal(50);
+    });
+
+    it('should have a name', function() {
+      expect(traveler.name).to.be.equal("Morey Flanders");
+    });
+
+    it('should have a traveler type', function() {
+      expect(traveler.travelerType).to.be.equal("foodie");
+    });
+
+
 
 
 
