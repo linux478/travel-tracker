53bf7ac4680343f5d5885edbff45b2f33bca373b
Susanna Carey <susannaopal@gmail.com>
2021-11-13 15:30:05 -0500

    Create destination id and add to trip data

--- test/Trips-test.js
+++ test/Trips-test.js
@@ -5,6 +5,7 @@ import Trip from '../src/classes/Trip.js';
 describe('Trip', () => {
   let tripData;
   let trip;
+  let destinationData
 
   beforeEach(function() {
     tripData = {
@@ -17,8 +18,17 @@ describe('Trip', () => {
       "status": "approved",
       "suggestedActivities": []
  };
-  trip = new Trip(tripData)
-});
+  destinationData = {
+      "id": 1,
+      "destination": "Lima, Peru",
+      "estimatedLodgingCostPerDay": 70,
+      "estimatedFlightCostPerPerson": 400,
+      "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+      "alt": "overview of city buildings with a clear sky"
+      };
+        trip = new Trip(tripData)
+        
+      });
 
  it('should be a function', function() {
    expect(Trip).to.be.a('function');
@@ -27,4 +37,4 @@ describe('Trip', () => {
   it('should institate an instance of Trip', function() {
     expect(trip).to.be.instanceOf(Trip);
   });
-})
+});
